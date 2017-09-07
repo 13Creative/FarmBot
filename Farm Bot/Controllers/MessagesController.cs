9c201c235e08f08c19d48d1996a80c1f419b6e7c
Jc Cloete <kazukix.kirigaya@gmail.com>
2017-09-07 21:00:25 +0200

    Main LUIS Integration.

--- Farm Bot/Controllers/MessagesController.cs
+++ Farm Bot/Controllers/MessagesController.cs
@@ -5,57 +5,81 @@ using System.Web.Http;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using Farm_Bot.Dialogs;
+using System.Diagnostics;
 
 namespace Farm_Bot
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
-        /// <summary>
-        /// POST: api/Messages
-        /// Receive a message from a user and reply to it
-        /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
-        {
-            if (activity.Type == ActivityTypes.Message)
-            {
-                await Conversation.SendAsync(activity, () => new RootDialog());
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+        internal static IDialog<object> MakeRoot() {
+            return Chain.From(() => new RootDialogALuis());
         }
 
-        private Activity HandleSystemMessage(Activity message)
-        {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
+        [System.Web.Http.Description.ResponseType(typeof(void))]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity) {
+            if (activity != null) {
+                switch (activity.GetActivityType()) {
+                    case ActivityTypes.Message:
+                        await Conversation.SendAsync(activity, MakeRoot);
+                        break;
+                    case ActivityTypes.ConversationUpdate:
+                    case ActivityTypes.ContactRelationUpdate:
+                    case ActivityTypes.Typing:
+                    case ActivityTypes.DeleteUserData:
+                    default:
+                        Trace.TraceError($"Unknown Activity Type Ignored : {activity.GetActivityType()}");
+                        break;
+                }
             }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
-
-            return null;
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
         }
+
+        ///// <summary>
+        ///// POST: api/Messages
+        ///// Receive a message from a user and reply to it
+        ///// </summary>
+        //public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        //{
+        //    if (activity.Type == ActivityTypes.Message)
+        //    {
+        //        await Conversation.SendAsync(activity, () => new RootDialogALuis());
+        //    }
+        //    else
+        //    {
+        //        HandleSystemMessage(activity);
+        //    }
+        //    var response = Request.CreateResponse(HttpStatusCode.OK);
+        //    return response;
+        //}
+
+        //private Activity HandleSystemMessage(Activity message)
+        //{
+        //    if (message.Type == ActivityTypes.DeleteUserData)
+        //    {
+        //        // Implement user deletion here
+        //        // If we handle user deletion, return a real message
+        //    }
+        //    else if (message.Type == ActivityTypes.ConversationUpdate)
+        //    {
+        //        // Handle conversation state changes, like members being added and removed
+        //        // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
+        //        // Not available in all channels
+        //    }
+        //    else if (message.Type == ActivityTypes.ContactRelationUpdate)
+        //    {
+        //        // Handle add/remove from contact lists
+        //        // Activity.From + Activity.Action represent what happened
+        //    }
+        //    else if (message.Type == ActivityTypes.Typing)
+        //    {
+        //        // Handle knowing tha the user is typing
+        //    }
+        //    else if (message.Type == ActivityTypes.Ping)
+        //    {
+        //    }
+
+        //    return null;
+        //}
     }
 }
---
+++ Farm Bot/Dialogs/RootDialogALuis.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+#pragma warning disable 1998
+
+namespace Farm_Bot.Dialogs
+{
+    [LuisModel("6b7b0ad8-5b3b-425a-bacc-08c79228c0f1", "3a452adbffff45209376addda3e04c1d", domain: "https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/6b7b0ad8-5b3b-425a-bacc-08c79228c0f1", staging: true)]
+    [Serializable]
+    public class RootDialogALuis : LuisDialog<object> {
+
+        [LuisIntent("LearnMore")]
+        public async Task GiveInfo(IDialogContext context, LuisResult result) {
+            await context.PostAsync("You Can Find Out More On Our Website, Located At : https://www.1point3creative.com");
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("")]
+        public async Task none(IDialogContext context, LuisResult result) {
+            await context.PostAsync("Sorry we didnt understand your message, please try again.");
+            context.Wait(MessageReceived);
+        }
+    }    
+}
